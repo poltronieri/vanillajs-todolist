d1ff0967ee2d52d671effe8468cdeee09a4a729c
gpoltronieri <guilherme.moreno@synchro.com.br>
2017-03-07 20:43:03 -0300

    alteraçoes nas funcoes.

--- js/app.js
+++ js/app.js
@@ -1,34 +1,18 @@
-const addTodoItem = function todoItem() {
-  var myList = document.querySelector("#myList");
-  var task = document.querySelector("#task");
-  if(canAddItem(myList, task)){
-  myList.innerHTML += "<li>" + task.value +"</li>";
-  task.value = "";
-  }
+function addTask() {
+  let task = document.querySelector("#task").value;
+  canAddTask(task);
 
+  let taskItem = document.createElement('input');
+  taskItem.type = 'checkbox';
+  taskItem.id = task;
+  
+  let taskList = document.querySelector("#taskList");
+  taskList.appendChild(taskItem);
+  // task.value = "";
 }
 
-const canAddItem = function canAddItem(list, task) {
-  var errorContainer = document.querySelector("#errorContainer");
-
-    if(!!task.value){
-      if(myList.childElementCount > 0){
-        for(var i = 0; i <= myList.childElementCount - 1; i++){
-          if(task.value === myList.children[i].innerText){
-            errorContainer.innerHTML = "Já existe um item com essa descrição.";
-            return false
-          } else {
-            errorContainer.innerHTML = "Add com sucesso";
-            return true
-          }
-        }
-      }else {
-        errorContainer.innerHTML = "Add com sucesso";
-        return true;
-      }
-
-    }else {
-      errorContainer.innerHTML = "Campo task é de preenchimento obrigatorio";
-      return false;
-    }
+function canAddTask(task) {
+  if(task === '') { 
+    throw 'A task não pode ter o seu valor vazio'; 
+  }
 }
